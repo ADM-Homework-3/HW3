84f9d0c4aaa05bf6c61e44cefa9a75d049738e8f
juan_mata <juan.mnaranjo@hotmail.com>
2020-11-18 13:20:00 +0100

    Fix data parse to account for several characters and such that the tsv can read empty strings

--- data_collection.py
+++ data_collection.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 import csv
+from langdetect import detect
+import io
 
 month_to_number = {'January': 1,
                    'February': 2,
@@ -23,13 +25,6 @@ def parse_html_in_folder(path):
         with open(path + '/' + html_file, encoding='utf8') as infile:
             print(html_file)
             soup = BeautifulSoup(infile, features="lxml")
-            bookTitle = soup.find_all('h1')[0].contents[0].replace('\n', '').strip()
-            bookSeries = soup.find_all('h2', id='bookSeries')[0].text.replace('\n', '').strip()
-            bookAuthors = soup.find_all('span', itemprop='name')[0].contents[
-                0]  # Review if it works with multiple authors as well
-            ratingValue = soup.find_all('span', itemprop='ratingValue')[0].contents[0].replace('\n', '').strip()
-            ratingCount = soup.find_all('meta', itemprop="ratingCount")[0]['content']
-            reviewCount = soup.find_all('meta', itemprop="reviewCount")[0]['content']
             # Plot can be hidden (if it is hidden we have to take the complete plot)
             try:
                 Plot = ' '.join([c for c in soup.find_all('div',
@@ -39,6 +34,15 @@ def parse_html_in_folder(path):
                 Plot = ' '.join([c for c in soup.find_all('div',
                                                           id="description")[0].contents[1].contents if
                                  isinstance(c, str)])
+            if detect(Plot) != 'en':
+                continue
+            bookTitle = soup.find_all('h1')[0].contents[0].replace('\n', '').strip()
+            bookSeries = soup.find_all('h2', id='bookSeries')[0].text.replace('\n', '').strip()
+            bookAuthors = ', '.join([soup.find_all('span', itemprop='name')[i].contents[0] for i in range(
+                len(soup.find_all('span', itemprop='name')))])
+            ratingValue = soup.find_all('span', itemprop='ratingValue')[0].contents[0].replace('\n', '').strip()
+            ratingCount = soup.find_all('meta', itemprop="ratingCount")[0]['content']
+            reviewCount = soup.find_all('meta', itemprop="reviewCount")[0]['content']
             NumberofPages = re.findall(r'\d+', soup.find_all('span', itemprop="numberOfPages")[0].contents[0])[0]
             # Shall we remove the parenthesis? (e.g 'Portia (hunger Games)')
             temp_date = soup.find_all('div', id='details')[0].find_all('div', {"class": "row"})[1].text.split('\n')[
@@ -58,22 +62,14 @@ def parse_html_in_folder(path):
                     characters.append(soup.find_all('div', id="bookDataBox")[0].find_all('a')[i].text)
                 elif re.match(r'/places/', soup.find_all('div', id="bookDataBox")[0].find_all('a')[i].attrs['href']):
                     settings.append(soup.find_all('div', id="bookDataBox")[0].find_all('a')[i].text)
+            characters = ', '.join(characters)
+            settings = ', '.join(settings)
             url = soup.find_all('link', rel='canonical')[0].attrs['href']
-            print(bookTitle)
-            print(bookSeries)
-            print(bookAuthors)
-            print(ratingValue)
-            print(ratingCount)
-            print(reviewCount)
-            print(Plot)
-            print(NumberofPages)
-            print(PublishingDate)
-            print(characters)
-            print(settings)
-            print(url)
-            print('=============')
 
-            with open(path + '/book' + re.findall(r'\d+', html_file)[0] + '.tsv', 'wt', newline='') as out_file:
+            final_list = [bookTitle, bookSeries, bookAuthors, ratingValue, ratingCount, reviewCount,
+                          Plot, NumberofPages, PublishingDate, PublishingDate, characters, settings, url]
+
+            with io.open('data/tsv_files/book' + re.findall(r'\d+', html_file)[0] + '.tsv', 'wt',
+                         newline='', encoding="utf-8") as out_file:
                 tsv_writer = csv.writer(out_file, delimiter='\t')
-                tsv_writer.writerow([bookTitle, bookSeries, bookAuthors, ratingValue, ratingCount, reviewCount,
-                                     Plot, NumberofPages, PublishingDate, PublishingDate, characters, settings, url])
+                tsv_writer.writerow(final_list)
